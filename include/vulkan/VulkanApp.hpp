fc0c672f39679e322130be6e83436577a59bdf64
Innoky <romanovinno@gmail.com>
2025-04-02 12:59:10 +0300

    createLogical Device added

--- include/vulkan/VulkanApp.hpp
+++ include/vulkan/VulkanApp.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <iostream>
 #include <optional>
+#include <set>
 
 struct QueueFamilyIndices
 {
--- src/vulkan/VulkanApp.cpp
+++ src/vulkan/VulkanApp.cpp
@@ -104,10 +104,53 @@ bool VulkanApp::isDeviceSuitable(VkPhysicalDevice device)
     return indices.isComplete();
 }
 
+void VulkanApp::createLogicalDevice()
+{
+    QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+    std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+    std::set<uint32_t> uniqueQueueFamilies = {
+        indices.graphicsFamily.value(),
+        indices.presentFamily.value()};
+
+    float queuePriority = 1.0f;
+
+    for (uint32_t queueFamily : uniqueQueueFamilies)
+    {
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = queueFamily;
+        queueCreateInfo.queueCount = 1;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+        queueCreateInfos.push_back(queueCreateInfo);
+    }
+
+    VkDeviceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+    createInfo.pQueueCreateInfos = queueCreateInfos.data();
+    createInfo.enabledExtensionCount = 0;
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed to create logical device!");
+    }
+
+    vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+    vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
+}
+
+void VulkanApp::createSwapchain()
+{
+    
+}
+
 void VulkanApp::initVulkan()
 {
     createInstance();
     createSurface();
     pickPhysicalDevice();
-    
+    createLogicalDevice();
+    createSwapchain();
 }
